2457d1a188f419c8bc2f0fe45a6eebe6ea978e84
Federico Lazarte <fedelazarte2015@gmail.com>
2025-08-21 11:01:26 -0300

    subiendo json y nosql

---
+++ nosql/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	bolt "go.etcd.io/bbolt"
+	"log"
+	"strconv"
+)
+
+type Alumne struct {
+	Legajo   int
+	Nombre   string
+	Apellido string
+}
+
+// Write Bucket
+func CreateUpdate(db *bolt.DB, bucketName string, key []byte, val []byte) error {
+	// abre transacción de escritura
+	tx, err := db.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	b, _ := tx.CreateBucketIfNotExists([]byte(bucketName))
+
+	err = b.Put(key, val)
+	if err != nil {
+		return err
+	}
+
+	// Cierra transacción
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Read from buckets
+func ReadUnique(db *bolt.DB, bucketName string, key []byte) ([]byte, error) {
+	var buf []byte
+
+	// abre una transacción de lectura
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		buf = b.Get(key)
+		return nil
+	})
+
+	return buf, err
+}
+
+func main() {
+	db, err := bolt.Open("guarani.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	cristina := Alumne{1, "Cristina", "Kirchner"}
+	data, err := json.Marshal(cristina)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	CreateUpdate(db, "alumne", []byte(strconv.Itoa(cristina.Legajo)), data)
+
+	resultado, err := ReadUnique(db, "alumne", []byte(strconv.Itoa(cristina.Legajo)))
+
+	fmt.Printf("%s\n", resultado)
+}
