42f0121493e2f116de62fb50914bd5b644c570ae
Federico Lazarte <fedelazarte2015@gmail.com>
2025-09-30 14:01:07 -0300

    add range

--- bucles/main.go
+++ bucles/main.go
@@ -22,6 +22,8 @@ import "fmt"
 * componentes de la declaración for y las llaves {} son siempre requeridas.
 * */
 
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
 func main() {
 	sum := 0
 	for i := 0; i < 10; i++ {
@@ -55,4 +57,36 @@ func main() {
 	* forma compacta.
 	* */
 	// for {}
+
+	/*
+	* Range
+	*
+	* El range de un for itera sobre un slice o map.
+	*
+	* Cuando se itera sobre un slice, se retornan dos valores por cada iteración. El primero es el índice, y el segundo
+	* es una copia del elemento en ese índice.
+	* */
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+
+	/*
+	* Range continuo
+	*
+	* Puedes omitir el índice o el valor asignado a _.
+	*
+	* for i, _ := range pow
+	* for _, value := range pow
+	*
+	* Si solo quieres el índice, puedes omitir la segunda variable.
+	*
+	* for i := range pow
+	* */
+	pow2 := make([]int, 10)
+	for i := range pow2 {
+		pow2[i] = 1 << uint(i) // 2**i
+	}
+	for _, value := range pow2 {
+		fmt.Printf("%d\n", value)
+	}
 }
