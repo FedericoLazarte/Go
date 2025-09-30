46499742298b6d494377bd5e34384862e8603616
Federico Lazarte <fedelazarte2015@gmail.com>
2025-09-30 12:19:32 -0300

    add arrays

--- arrays/main.go
+++ arrays/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+/*
+* Arrays
+*
+* El tipo [n]T es un array de n valores de tipo T.
+*
+* La expresión
+*
+* var a [10]int
+*
+* declara una variale a como un array de diez enteros.
+*
+* El tamaño de un array es parte de su tipo, por lo que los arrays no se pueden redimensionar. Esto parece limitante, pero no
+* te preocupes; Go provee una forma conveniente de trabajar con arrays.
+* */
+
 func main() {
 	var a [2]string
 	a[0] = "Hola"
