02b41bce209e8f8ddbc3a27f3a417fb732b23e9c
Federico Lazarte <fedelazarte2015@gmail.com>
2025-09-30 01:54:34 -0300

    more more

--- variables/main.go
+++ variables/main.go
@@ -18,6 +18,36 @@ var c, python, java bool
 * Si hay un inicializador, se puede omitir el tipo; la variable tomará el tipo inicializador.*/
 var i2, j2 int = 1, 2
 
+/*
+* Inferencia de tipos
+*
+* Al declarar una variable sin especificar un tipo explícito (ya sea utilizando la sintaxis de expresión := 0 var =),
+* el tipo de variable se deduce del valor del lado derecho.
+*
+* Cuando se escribe el lado derecho de la declaración, la nueva variable es del mismo tipo:
+*
+* var i int
+* j := i // j es int
+*
+* Pero cuando el lado derecho contiene una constante numérica sin tipo, la nueva riable puede ser int, float64 o
+* complex128 dependiendo de la precisión de la constante:
+*
+* i := 42 // int
+* f := 3.142 // float64
+* g := 0.867 + 0.5i // complex128
+* */
+
+/*
+* constantes
+*
+* Las constantes se declarando como variables, pero con la palabra clave const.
+*
+* Las constantes pueden ser caracteres, cadenas de texto, booleanos o valores numéricos.
+*
+* Las constantes no se pueden declarar usando la sintaxis :=
+* */
+const Pi = 3.14
+
 func main() {
 	var i int
 	fmt.Println(i, c, python, java)
@@ -49,4 +79,6 @@ func main() {
 	var b bool
 	var s string
 	fmt.Printf("%v %v %v %q\n", i4, f, b, s)
+
+	fmt.Println("Constante:", Pi)
 }
