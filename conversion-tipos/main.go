823c959f66c705fa25ca4f2987422ceed942bdf7
Federico Lazarte <fedelazarte2015@gmail.com>
2025-09-30 01:56:06 -0300

    more more more

--- conversion-tipos/main.go
+++ conversion-tipos/main.go
@@ -5,6 +5,15 @@ import (
 	"math"
 )
 
+/*
+* La expresión T(v) convierte el valor v en T.
+*
+* Algunas conversiones numéricas
+*
+* A diferencia de C, en Go la asignación entre elementos de diferentes tipos requiere una conversión
+* explicita.
+* */
+
 func main() {
 	var x, y int = 3, 4
 	var f float64 = math.Sqrt(float64(x*x + y*y))
