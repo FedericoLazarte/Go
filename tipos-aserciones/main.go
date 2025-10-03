cb02965b8d435bc7a2533b5ebf386994031980a4
Federico Lazarte <fedelazarte2015@gmail.com>
2025-10-03 17:51:12 -0300

    fix assertions

--- tipos-aserciones/main.go
+++ tipos-aserciones/main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+/*
+* Tipo aserciones
+*
+* Una aserción de tipo proporciona acceso al valor concreto subyacente de un valor de interfaz.
+*
+* t := i.(T)
+*
+* Esta declaración firma que el valor de la interfaz i sostiene el tipo concreto T y asigna
+* el valor subyacente T a la variable t.
+*
+* Si i no contiene a T, la declaración provocará panic.
+*
+* Para probar si un valor de interfaz contiene un tipo especifico, una aserción de tipo puede devolver dos
+* valores: el valor subyacente y un valor booleano que informa si la afirmación tuvo éxito.
+*
+* t, ok := i.(T)
+*
+* Si i contiene a T, entonces t será el valor subyacente y ok será verdadero.
+*
+* Si no, ok será falso y t será el valor cero del tipo T, y ningún panic ocurrira.
+*
+* Tenga en cuenta la similitud entre esta sintaxis y la de leer un map.
+* */
+
 func main() {
 	var i interface{} = "hola"
 
